d1faafe968cead4b11b30509bf2dc64c11e858c9
jjmuchai <jjmuchai151@gmail.com>
2025-03-26 06:12:07 +0300

    My updates to the project

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error fetching books:', error));
 }
 
 function renderBooks(books) {
